ec016ab171eb6406bf42f22e27503ffa43f487bb
Daniela Muñoz Valdés <daniela.constanza03@gmail.com>
2025-08-06 14:17:55 -0400

    Revert "Merge pull request #5 from rodri-fullstack/rodri-database"
    This reverts commit c3bcab6a4122f113a26fd68689d68acffbabe206, reversing
    changes made to 57d359433f2a13837a33c8185f09b9ad2d2ccecd.

--- tiendita/src/main/java/com/bootcamp/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bootcamp.repository;
-
-import com.bootcamp.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
