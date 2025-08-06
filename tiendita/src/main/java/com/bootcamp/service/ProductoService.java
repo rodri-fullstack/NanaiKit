d9bbf2a49b8f8e957851678bcce6e258dc687aa4
Kinerodri <154571538+Kinerodri@users.noreply.github.com>
2025-08-06 12:19:34 -0400

    subo estructura springboot database

---
+++ tiendita/src/main/java/com/bootcamp/service/ProductoService.java
@@ -0,0 +1,14 @@
+package com.bootcamp.service;
+
+import com.bootcamp.model.Producto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoService {
+    List<Producto> listarTodos();
+    Optional<Producto> obtenerPorId(Long id);
+    Producto guardar(Producto producto);
+    Producto actualizar(Long id, Producto producto);
+    void eliminar(Long id);
+}
