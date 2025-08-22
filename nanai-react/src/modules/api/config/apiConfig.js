28d6dec62026cfac3d1285c61ec465668404dc7f
Kinerodri <154571538+Kinerodri@users.noreply.github.com>
2025-08-22 11:25:12 -0400

    subo render al frontend

--- nanai-react/src/modules/api/config/apiConfig.js
+++ nanai-react/src/modules/api/config/apiConfig.js
@@ -1,7 +1,7 @@
 // Configuración de la API
 
 // URL base de la API
-export const API_URL = 'https://nanaikit-j1c8.onrender.com';
+export const API_URL = 'https://nanaikit-5ok0.onrender.com';
 
 // Configuración por defecto para Axios
 export const API_CONFIG = {
